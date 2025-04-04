b6f0edbe4d2d7a90179dcdb73c0f7a9a43a8ebcf
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-04-04 22:01:13 +0300

    Added login function

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -37,4 +37,37 @@ class AuthController extends Controller
             );
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $credentials = [
+                "email" => $request["email"],
+                "password" => $request["password"]
+            ];
+            if (!$token = Auth::attempt($credentials)) {
+                return responseMessage(
+                    false,
+                    401,
+                    "Email or password not correct"
+                );
+
+            }
+            $user = Auth::user();
+            $user->token = $token;
+
+            return responseMessage(
+                true,
+                200,
+                "Login successfully, Welcome " . $user->full_name,
+                $user
+            );
+        } catch (\Throwable $e) {
+            return responseMessage(
+                false,
+                401,
+                $e->getMessage()
+            );
+        }
+    }
 }
