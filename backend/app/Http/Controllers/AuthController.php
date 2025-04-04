3e5bb1d27def0eea66e85e65ab7180e1e4adb1c7
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-04-04 21:59:39 +0300

    Added signup function

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -8,5 +8,33 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(Request $request)
+    {
+        try {
+            $request->validate([
+                'full_name' => 'required|string|max:50',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+            $user = new User;
+            $user->full_name = $request["full_name"];
+            $user->email = $request["email"];
+            $user->password = bcrypt($request["password"]);
+            $user->save();
+            $user->token = Auth::login($user);
+
+            return responseMessage(
+                true,
+                200,
+                "Signup successfully, Welcome " . $user->full_name,
+                $user
+            );
+        } catch (\Throwable $e) {
+            return responseMessage(
+                false,
+                401,
+                $e->getMessage()
+            );
+        }
+    }
 }
